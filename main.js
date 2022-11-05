c10ec6952f911cd8b51bd1adcd656fd72c3a873c
MorninDue <100368985+MorninDue@users.noreply.github.com>
2022-11-05 11:29:43 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,106 @@
+var SpeechRecognition = window.webkitSpeechRecognition;
+  
+var recognition = new SpeechRecognition();
+
+function start()
+{
+    document.getElementById("textbox").innerHTML = ""; 
+    recognition.start();
+} 
+ 
+recognition.onresult = function(event) {
+
+ console.log(event); 
+
+var Content = event.results[0][0].transcript;
+
+    document.getElementById("textbox").innerHTML = Content;
+    console.log(Content);
+      if(Content =="take my selfie")
+      {
+        console.log("taking selfie --- ");
+        speak();
+      }
+}
+
+
+function speak(){
+    var synth = window.speechSynthesis;
+
+    speak_data = "Taking you Selfie in 5 seconds";
+
+    var utterThis = new SpeechSynthesisUtterance(speak_data);
+
+    synth.speak(utterThis);
+
+    Webcam.attach(camera);
+
+    setTimeout(function()
+    { 
+        take_snapshot(); 
+        save();
+    }, 5000);
+}
+
+ 
+camera = document.getElementById("camera");
+Webcam.set({
+    width:360,
+    height:250,
+    image_format : 'jpeg',
+    jpeg_quality:90
+});
+
+function take_snapshot()
+{
+    Webcam.snap(function(data_uri) {
+        if(img_id=="selfie1"){
+            document.getElementById("result1").innerHTML = '<img id="selfie1" src='+data_uri+'"/>';
+        }
+
+        if(img_id=="selfie2"){
+            document.getElementById("result2").innerHTML = '<img id="selfie2" src='+data_uri+'"/>';
+        }
+
+        if(img_id=="selfie3"){
+            document.getElementById("result3").innerHTML = '<img id="selfie3" src='+data_uri+'"/>';
+        }
+
+    });
+}
+
+
+function save()
+{
+  link = document.getElementById("link");
+  image = document.getElementById("selfie_image").src ;
+  link.href = image;
+  link.click();
+}
+
+setTimeout(function()
+{
+    img_id = "selfi1";
+    take_snapshot();
+    speak_data = "Taking Your next Selfie in 10 seconds";
+    var utterThis = new SpeechSynthesisUtterance(speak_data);
+    synth.speak(utterThis);
+},5,000)
+
+setTimeout(function()
+{
+    img_id = "selfi2";
+    take_snapshot();
+    speak_data = "Taking Your next Selfie in 10 seconds";
+    var utterThis = new SpeechSynthesisUtterance(speak_data);
+    synth.speak(utterThis);
+},5,000)
+
+setTimeout(function()
+{
+    img_id = "selfi3";
+    take_snapshot();
+    speak_data = "Taking Your next Selfie in 10 seconds";
+    var utterThis = new SpeechSynthesisUtterance(speak_data);
+    synth.speak(utterThis);
+},5,000)
